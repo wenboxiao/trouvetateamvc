d6ad5eea2577d964d422a7323925d12668dc4ad3
Halitim <raphael.halitim@isep.fr>
2016-05-30 17:07:49 +0200

    30_3

--- signalement.php
+++ signalement.php
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 <?php session_start(); ?>
 <?php 
 
@@ -31,47 +29,6 @@ else {
 						'id_objet' => $_POST['id_objet']
 						));
 	$req1->closeCursor();
-	include("back_office_traitement.php");
-}
-=======
-=======
->>>>>>> origin/master
-
-<?php session_start(); ?>
-<?php 
-
-try
-{
-$bdd = new PDO('mysql:host=localhost;dbname=trouve_ta_team;charset=utf8', 'root', '');
-}
-catch (Exception $e)
-{
-        die('Erreur : ' . $e->getMessage());
-}
-
-if (!isset($_SESSION['tttpseudo'])) {
-	include("Connexion.php");
-}
-else {
-	$reqa = $bdd->prepare('SELECT id_utilisateur FROM utilisateurs WHERE NomUtilisateur = ?');
-	$reqa->execute(array($_SESSION['tttpseudo']));
-	if ($donnee = $reqa->fetch()) {
-		$id_delateur=$donnee['id_utilisateur'];}
-		$reqa->closeCursor();
-	
-	$req1 = $bdd->prepare('INSERT INTO signalement_abus(motif_signalement, type_objet, id_delateur, id_utilisateur_denonce, id_objet) VALUES(:motif, :type_objet, :id_delateur, :id_utilisateur_denonce, :id_objet)');
-
-	$req1->execute(array('motif' => $_POST['rapportsignalement'],
-						'type_objet' => $_POST['type_objet'],
-						'id_delateur' => $id_delateur,
-						'id_utilisateur_denonce' => $_POST['utilisateur_denonce'],
-						'id_objet' => $_POST['id_objet']
-						));
-	$req1->closeCursor();
 	include("signalement_termine.php");
 }
-<<<<<<< HEAD
->>>>>>> origin/master
-=======
->>>>>>> origin/master
 ?>
--- signalement_termine.php
+++ signalement_termine.php
@@ -1,27 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html>
 
- 	<head>
-        <meta charset="utf-8" />
-        <link rel="stylesheet" href="stylettt.css" />
-        <link rel="stylesheet" href="Am.css" />
-        <link rel="icon" type="image/png" href="image/faviconessai.png" />
-        <title>Trouve ta Team</title>
-    </head>
-    <body>
-    	<?php include("Barred.php"); ?>
-        <p class="centrer"> Votre requête a bien été envoyée à l'administrateur, qui l'examinera <br/>
-        </p>
-        <div class="boutonp">
-            <h3><a href="Trouvetateam.php" class="bouton">Accueil</a></h3>
-        </div>
-        <?php include("Barrer.php"); ?>
-    </body>
-=======
-=======
->>>>>>> origin/master
 <!DOCTYPE html>
 <html>
 
@@ -46,8 +23,3 @@
         </div>
         <?php include("Barrer.php"); ?>
     </body>
-<<<<<<< HEAD
->>>>>>> origin/master
-=======
->>>>>>> origin/master
-</html>
