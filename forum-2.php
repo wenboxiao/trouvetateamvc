fe719e94b9d8c96b93c8e8c87702f1832a4b9bc0
Halitim <raphael.halitim@isep.fr>
2016-06-06 16:20:46 +0200

    forum_2

--- forum-2.php
+++ forum-2.php
@@ -1,11 +1,22 @@
 	<h3><p class="titrevert">Aide</p></h3>
 	<a class="bouton"href="?page=ajouter-sujet">Ajouter un sujet</a>
 	<ul>
+	<table  border="1"  bordercolor="blue" width="80%" align="center" cellspacing="10" cellpadding="10" color="blue">
+	<tr>
+		<td width=50%> Sujet </td>
+		<td width=30%>ecrit par :</td>
+		<td width=20%>Date </td>
+	</tr></table>
 	<?php
 		foreach ($sujets_aide as $sujet) {
-	?><table  border="1"  bordercolor="blue" width="80%" align="center">
-		<td><h2><a class="titrerouge" href="?page=sujet&id=<?php echo $sujet['id']; ?>"><?php echo $sujet['titre']; ?></a></h2></td>
+	?>
+<table  border="1"  bordercolor="blue" width="80%" align="center">
+	
+<tr>		<td><h2><a class="titrerouge" href="?page=sujet&id=<?php echo $sujet['id']; ?>"><?php echo $sujet['titre']; ?></a></h2></td>
+			<td width=30%><?php echo $sujet['id_membre'];?></td>
 			<td	width="20%"><?php echo $sujet['date']; ?></td>
+			
+</tr>
 		</table>
 	<?php
 		
