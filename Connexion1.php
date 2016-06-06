d82f0f01ddd5f798450026ca2181bde0727a3433
WaldPaul <pwaldburger@free.fr>
2016-06-06 17:12:58 +0200

    champ connexion updated

--- Connexion1.php
+++ Connexion1.php
@@ -13,9 +13,9 @@
         <div class="space"></div>     
         <form method="post" action="Pass.php">
                 <label for="tttpseudo" class="titrevert2">Votre pseudo :</label><br />
-                <div class="pss"><input type="text" name="tttpseudo" id="tttpseudo" class="tttpseudo" placeholder="Ex: Gladolferat" size="30" maxlength="30" /></div><br />
+                <div class="pss"><input type="text" name="tttpseudo" id="tttpseudo" class="tttpseudo" placeholder="Ex: Fred" size="30" maxlength="30" /></div><br />
                 <label for="tttpass" class="titrevert2">Votre mot de passe :</label><br />
-                <div class="pss"><input type="password" name="tttpass" id="tttpass" placeholder="Ex: CaputDrackonis" size="30" maxlength="30"/></div><br />
+                <div class="pss"><input type="password" name="tttpass" id="tttpass" placeholder="Ex: 1234" size="30" maxlength="30"/></div><br />
                 <h1 class="boutonp3"><input type="submit" value="Connexion"  class="bouton3"></h1></form>
                 <a class="titrerouge" href="reinitialisation_mot_de_passe.php">*Mot de passe oublié ?</a><br/>
         
